ff77c2671a81cfb8205ceef2f930a941a15478dc
Andreas Huber <info@ndhbr.de>
2021-11-20 14:37:34 +0100

    Updated tests

--- src/__tests__/index_test.js
+++ src/__tests__/index_test.js
@@ -1,15 +1,18 @@
 const { generatePyramid } = require('../index')
 
 test("generate pyramid function", () => {
-  expect(generatePyramid(1)).toBe(`#  #`)
+  expect(generatePyramid(1)).toBe(`#  #
+`)
   expect(generatePyramid(2)).toBe(` #  #
-  ##  ##`)
+##  ##
+`)
   expect(generatePyramid(8)).toBe(`       #  #
-  ##  ##
- ###  ###
-####  ####
-#####  #####
-######  ######
-#######  #######
-########  ########`)
+      ##  ##
+     ###  ###
+    ####  ####
+   #####  #####
+  ######  ######
+ #######  #######
+########  ########
+`)
 })
